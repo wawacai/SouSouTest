065973389660a4cbadcd651c2a081e733d48159a
彭作青 <pengzuoqing@163.com>
2017-01-04 11:43:40 +0800

    Initial Commit

---
+++ SouSouTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SouSouTest
+//
+//  Created by 彭作青 on 2017/1/4.
+//  Copyright © 2017年 myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SouSouTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SouSouTest
+//
+//  Created by 彭作青 on 2017/1/4.
+//  Copyright © 2017年 myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
