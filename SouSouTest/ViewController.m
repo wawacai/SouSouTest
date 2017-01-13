c554c22da4d2b409acf7a253dad621a3e705ec82
肖培德 <mac@xiaopeidedeMacBook-Pro.local>
2017-01-13 14:56:27 +0800

    修改人脸验证和身份验证逻辑

--- SouSouTest/ViewController.m
+++ SouSouTest/ViewController.m
@@ -17,26 +17,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"JYVivo" bundle:nil];
-    LoginController *loginVc = [sb instantiateInitialViewController];
-    //    [self presentViewController:loginVc animated:YES completion:nil];
-    [self.navigationController pushViewController:loginVc animated:YES];
 }
 - (IBAction)faceTest:(UIButton *)sender {
 //    
-//    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"JYVivo" bundle:nil];
-//    LoginController *loginVc = [sb instantiateInitialViewController];
-////    [self presentViewController:loginVc animated:YES completion:nil];
-//    [self.navigationController pushViewController:loginVc animated:YES];
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"JYVivo" bundle:nil];
+    LoginController *loginVc = [sb instantiateInitialViewController];
+//    [self presentViewController:loginVc animated:YES completion:nil];
+    [self.navigationController pushViewController:loginVc animated:YES];
     
-    SoSoIdCardVertifyViewController *vc = [[SoSoIdCardVertifyViewController alloc] init];
-    [self.navigationController pushViewController:vc animated:YES];
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
